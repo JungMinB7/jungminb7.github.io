66e65dbe6c8a8f186483943099b4414d981e75d4
JungMin <ljlm0829@gmail.com>
2025-01-26 10:46:27 +0900

    config update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737789540',resources: [
+    cacheName: 'chirpy-1737855602',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
