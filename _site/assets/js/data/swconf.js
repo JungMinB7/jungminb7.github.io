452eafcf6cbe0af51851d69d1f71c6f4fa28af52
JungMin <ljlm0829@gmail.com>
2025-01-26 18:45:12 +0900

    브랜치가 왜 안생기지

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737883791',resources: [
+    cacheName: 'chirpy-1737884687',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
