753d59cff223d27dff0dfd2c122ba90b652e39a7
JungMin <ljlm0829@gmail.com>
2025-01-26 11:32:58 +0900

    안되면 죽여버린다

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737855602',resources: [
+    cacheName: 'chirpy-1737858751',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
