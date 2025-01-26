31ff37efb91c3b440548b0d22d9afbb4604dc0bd
JungMin <ljlm0829@gmail.com>
2025-01-26 18:09:40 +0900

    comments tool

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737875393',resources: [
+    cacheName: 'chirpy-1737882055',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
