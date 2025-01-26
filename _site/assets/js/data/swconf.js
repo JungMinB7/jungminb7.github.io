dbde3e5edd63bd2e6e99ae13c57c809077a87ba4
JungMin <ljlm0829@gmail.com>
2025-01-26 13:49:52 +0900

    logo upload

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737858751',resources: [
+    cacheName: 'chirpy-1737866776',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
