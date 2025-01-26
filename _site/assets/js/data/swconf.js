255eacbdbfec8f4a6e7e5c3b6221ec38c12a46a5
JungMin <ljlm0829@gmail.com>
2025-01-26 20:35:54 +0900

    plese it works

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737884687',resources: [
+    cacheName: 'chirpy-1737891258',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
