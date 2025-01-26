f7e9c8dd44f52b01b0b7b0485fe71f29c71aa29f
JungMin <ljlm0829@gmail.com>
2025-01-26 14:40:11 +0900

    왜 안돼 미친

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737867783',resources: [
+    cacheName: 'chirpy-1737869635',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
