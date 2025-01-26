447726913d6300f49ee628ce370c5eb83ba95a81
JungMin <ljlm0829@gmail.com>
2025-01-26 18:33:24 +0900

    gitignore

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737882055',resources: [
+    cacheName: 'chirpy-1737883791',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
